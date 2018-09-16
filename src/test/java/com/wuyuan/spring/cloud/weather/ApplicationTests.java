179e50d6bfc95b0e7fbca04125e252f65a222186
Administrator <Administrator@PC-20160114JFOU>
2018-09-16 10:29:12 +0800

    firstbuild

---
+++ src/test/java/com/wuyuan/spring/cloud/weather/ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.wuyuan.spring.cloud.weather;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
